a91f25ed8d7e0ee7ad6ac2ebe7983d024030dda4
Liliankrf <54894606+Liliankrf@users.noreply.github.com>
2019-12-19 15:02:42 +0100

    update

---
+++ include/html.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='./../assets/css/style.css'>
+    <title>Document</title>
+</head>
+
+<header id="sommaire">
+        <ul>
+            <li> Acceuil </li>
+            <li> Contact </li>
+            <li> Inscription </li>
+            <li> Mentions Légales </li>
+        </ul>
+    <title>NWS-PHP</title>
+</header>
+
+<body>
+
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-
+require './include/html.php';
+require './include/header.php';
+require './include/main.php';
+require './include/footer.php';
